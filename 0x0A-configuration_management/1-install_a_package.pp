18db8a41212ac745697c2765a2d6b22ca9b6d5e4
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-23 05:36:23 +0300

    Indentation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,8 +5,8 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-    command     => '/usr/bin/pip3 install Flask==2.1.0',
-    path        => ['/usr/bin'],
-    creates     => '/usr/local/bin/flask',
-    require     => Package['python3-pip'],
+    command => '/usr/bin/pip3 install Flask==2.1.0',
+    path    => ['/usr/bin'],
+    creates => '/usr/local/bin/flask',
+    require => Package['python3-pip'],
 }
