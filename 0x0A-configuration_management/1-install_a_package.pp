c74dc8c6f2962088b3055bf0dd5ea01c5fa5b535
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-23 05:40:07 +0300

    Changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -7,6 +7,5 @@ package { 'python3-pip':
 exec { 'install_flask':
     command => '/usr/bin/pip3 install Flask==2.1.0',
     path    => ['/usr/bin'],
-    creates => '/usr/local/bin/flask',
     require => Package['python3-pip'],
 }
