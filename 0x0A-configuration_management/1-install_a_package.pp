7ec867b112ec2139fe36350670cfdc157684666c
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-23 05:46:51 +0300

    Using Puppet, create a manifest that kills a process named killmenow.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,12 @@
 # Install flask from pip3
 
-package { 'python3-pip':
-    ensure  => installed,
-}
-
 exec { 'install_flask':
     command => '/usr/bin/pip3 install Flask==2.1.0',
     path    => ['/usr/bin'],
+    creates => '/usr/local/bin/flask',
     require => Package['python3-pip'],
+}
+
+package { 'python3-pip':
+    ensure  => installed,
 }
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# kills a process named killmenow
+
+exec { 'kill_killmenow_process':
+    command => 'pkill -f killmenow',
+    path    => ['/bin', '/usr/bin'],
+}
