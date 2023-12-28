5ebe1a237603ae327165aaeeeae03fff4299a81f
Epaphradito Lugayavu <paphra.me@gmail.com>
2023-12-28 13:49:57 +0300

    Changes

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,26 +1,18 @@
 # Puppet file for managing the SSH Client configuration
 
-class ssh_config {
-    $ssh_config_file = "/etc/ssh/ssh_config"
-
-    file { $ssh_config_file:
-        ensure  => present,
-    }
-
-    file_line { 'IdentityFile':
-        path    => $ssh_config_file,
-        line    => '    IdentityFile ~/.ssh/school',
-        match   => '^#? *IdentityFile',
-        ensure  => present,
-    }
-
-    file_line { 'PasswordAuthentication':
-        path    => $ssh_config_file,
-        line    => '    PasswordAuthentication no',
-        match   => '^#? *PasswordAuthentication',
-        ensure  => present,
-    }
-
+$ssh_config_file = '/etc/ssh/ssh_config'
+file { $ssh_config_file:
+    ensure  => present,
 }
-
-include ssh_config
+file_line { 'IdentityFile':
+    ensure => present,
+    path   => $ssh_config_file,
+    line   => '	IdentityFile ~/.ssh/school',
+    match  => '^#? *IdentityFile',
+}
+file_line { 'PasswordAuthentication':
+    ensure => present,
+    path   => $ssh_config_file,
+    line   => '    PasswordAuthentication no',
+    match  => '^#? *PasswordAuthentication',
+}
