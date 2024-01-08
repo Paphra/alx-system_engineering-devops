82236051181cafaf394f89e6a5c1fbd259760811
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-01-08 15:39:41 +0300

    Changes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,31 +1,43 @@
 # Setup the nginx server using puppet manifest
 
-class { 'nginx':
-  ensure => 'installed',
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-  notify  => Service['nginx'],
-}
-
-nginx::resource::vhost { 'redirect_me':
-  www_root     => '/var/www/html',
-  listen_port  => 80,
-  server_name  => 'localhost',
-  redirect_dest => 'https://www.youtube.com/watch?v=QH2-TGUlwu4',
-  redirect_type => 'Permanent',
+package { 'nginx':
+    ensure => 'installed',
 }
 
 service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => [File['/etc/nginx/sites-available/default'], File['/var/www/html/index.html']],
+    ensure  => running,
+    enable  => true,
+    require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+    ensure  => 'present',
+    content => '# File: templates/default.erb
+    server {
+        listen 80;
+        root /var/www/html;
+        index index.html;
+
+        location = /404.html {
+            root /var/www/html;
+            internal;
+        }
+
+        error_page 404 /404.html;
+
+        location /redirect_me {
+            return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+    }',
+    notify  => Service['nginx'],
+}
+
+file { '/var/www/html/index.html':
+    ensure  => 'present',
+    content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+    ensure  => 'present',
+    content => "Ceci n'est pas une page",
 }
