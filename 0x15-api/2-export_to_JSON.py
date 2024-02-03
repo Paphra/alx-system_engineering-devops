821cb2bdcbf17a9672b1c960d057a6d9829a80ee
Epaphradito Lugayavu <paphra.me@gmail.com>
2024-02-03 12:11:37 +0300

    Changes to the third task, export JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""1-export_to_CSV module
+"""0-gather_data_from_an_API module
 For a rest api that fetched employee data
 """
-import csv
-import json
 import requests
 import sys
+import json
 
 
 def employee_todo_progress(emp_id):
@@ -13,26 +12,28 @@ def employee_todo_progress(emp_id):
     """
     base_url = "https://jsonplaceholder.typicode.com"
     user_url = "{}/users/{}".format(base_url, emp_id)
-    todos_url = "{}/todos/{}".format(base_url, emp_id)
+    todos_url = "{}/todos?userId={}".format(base_url, emp_id)
 
     user_response = requests.get(user_url)
     user_data = user_response.json()
-    user_id = user_data.get('id')
     emp_name = user_data.get('name')
 
     todos_res = requests.get(todos_url)
     todos_data = todos_res.json()
 
-    csv_file_name = "{}.csv".format(user_id)
-    with open(csv_file_name, mode='w', newline='') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for task in json.loads(todos_data):
-            writer.writerow([
-                user_id,
-                emp_name,
-                "{:bool}".format(task['completed']),
-                task['title']
-            ])
+    export_data = {
+        str(emp_id): [
+            {
+                'task': task['title'],
+                'completed': task['completed'],
+                'username': emp_name
+            } for task in todos_data
+        ]
+    }
+
+    json_filename = '{}.json'.format(emp_id)
+    with open(json_filename, 'w') as json_file:
+        json.dump(export_data, json_file)
 
 
 if __name__ == '__main__':
